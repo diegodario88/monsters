74a88e3b0e80311cbcfee79159f6d7b3c9fbf344
Diego Dario <diegodario88@gmail.com>
2020-01-07 13:38:08 -0300

    Updates

---
+++ precache-manifest.64bebabc62765ecacb740dadf99a1973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da312b98e402d7e16a9ec159e6c6bb9d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "aef3cc01cf13233371f1",
+    "url": "/monsters/static/css/main.a062564c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aef3cc01cf13233371f1",
+    "url": "/monsters/static/js/main.92949a68.chunk.js"
+  },
+  {
+    "revision": "475d84c361081acc4a17",
+    "url": "/monsters/static/js/runtime-main.05e5ea3c.js"
+  }
+]);
